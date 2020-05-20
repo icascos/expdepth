f3d60f0f8c70aa04eee70108f48c9517c9b09932
Ignacio Cascos <62115195+icascos@users.noreply.github.com>
2020-05-20 10:36:15 +0200

    Update BExPlot.R

--- BExPlot.R
+++ BExPlot.R
@@ -1,11 +1,11 @@
 source("https://raw.githubusercontent.com/cran/expectreg/master/R/expectile.R")
 
 
-extreme.points <- function(data,n=1000,alpha=0.15){
-  angle <- seq(0,2*pi,length.out=n) 
+extreme.points <- function(data,nlines=1000,alpha=0.15){
+  angle <- seq(0,2*pi,length.out=nlines) 
   extreme.points <- NULL
   exp1 <- expectile(x=data[,1]*cos(angle[1])+data[,2]*sin(angle[1]),probs=alpha,dec=16)
-  for(i in 2:n){
+  for(i in 2:nlines){
     exp2 <-  expectile(x=data[,1]*cos(angle[i])+data[,2]*sin(angle[i]),probs=alpha,dec=16)
     extreme.points <- rbind(extreme.points,solve(matrix(c(cos(angle[i-1]),sin(angle[i-1]),
                                                           cos(angle[i]),sin(angle[i])),byrow=TRUE,ncol=2),
@@ -24,12 +24,12 @@ conf.level <- function(beta=.95,n=100) {
 }
 
 
-BExPlot <- function(data,n=1000,conf=0.95) {
+BExPlot <- function(data,nlines=1000,conf=0.95) {
   plot(data,type="n",main="BExPlot",xlab="X",ylab="Y")
   av <- colMeans(data)
-  bag <- extreme.points(data,n=n)
+  bag <- extreme.points(data,nlines=nlines)
   alpha.conf <- conf.level(beta=conf,n=nrow(data))
-  conf.region <- extreme.points(data,n=n,alpha=alpha.conf)
+  conf.region <- extreme.points(data,nlines=nlines,alpha=alpha.conf)
   expanded <- t(4*(t(bag)-av)+av)
   polygon(bag,col="light grey",border=FALSE)
   lines(bag,lty=2)
